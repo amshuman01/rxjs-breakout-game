954ecad4e07fe77917ceac5207907dd96e803e43
amshuman01 <amshuman.hegde@robosoftin.com>
2023-03-25 20:58:11 +0530

    comments

--- html-renderer.ts
+++ html-renderer.ts
@@ -1,12 +1,13 @@
 import { gameSize } from "./constants";
 import { Player, Ball, GameObject } from "./interfaces";
 
-const empty = 0;
+const empty = 0; // diff types of board elements
 const plyer = 1;
 const bll = 2;
 const brick = 3;
 
 const createElem = (col) => {
+  // creates background with specified features
   const elem = document.createElement("div");
   elem.classList.add("board");
   elem.style.display = "inline-block";
@@ -25,6 +26,7 @@ const createElem = (col) => {
   return elem;
 };
 
+// takes current state of player ball and game obj and renders to browser
 export const render = ([player, ball, bricks]: [
   Player,
   Ball,
@@ -32,13 +34,15 @@ export const render = ([player, ball, bricks]: [
 ]) => {
   const game = Array(gameSize)
     .fill(0)
-    .map((e) => Array(gameSize).fill(0));
-  game[player.x][player.y] = plyer;
-  game[ball.x][ball.y] = bll;
-  bricks.forEach((b) => (game[b.x][b.y] = brick));
+    .map((e) => Array(gameSize).fill(0)); //game board with empty spaces
+  game[player.x][player.y] = plyer; //player pos on the game board
+  game[ball.x][ball.y] = bll; //sets ball pos on the game board
+  bricks.forEach((b) => (game[b.x][b.y] = brick)); // brick pos
 
+  // diaply lives , score  in inner html
   document.body.innerHTML = `Score: ${player.score} Lives: ${player.lives} <br/>`;
   game.forEach((r) => {
+    //creates row container div and fills it based on game array
     const rowContainer = document.createElement("div");
     r.forEach((c) => rowContainer.appendChild(createElem(c)));
     document.body.appendChild(rowContainer);
